5d60fa55c5b271cef6d63ca56fcc12d4af1bc230
Marcelo Colla <mcolla.dev@gmail.com>
2022-02-04 08:31:29 -0300

    setup initial project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+
+module.exports = {
+  entry: "./index.js",
+  mode: "development",
+  devtool: "hidden-source-map",
+  output: {
+    publicPath: "http://localhost:3000/",
+    clean: true,
+  },
+  module: {},
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "shellMfe",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./react": "react",
+        "./react-dom": "react-dom",
+        "./react-router-dom": "react-router-dom",
+        "./@material-ui/core": "@material-ui/core",
+        "./@material-ui/icons": "@material-ui/icons",
+      },
+    }),
+  ],
+};
